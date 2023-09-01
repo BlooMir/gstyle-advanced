f53964cdfc02ad3bdd9ed5771391584177b81356
BlooMir <binarywaterdragon@gmail.com>
2023-09-01 17:27:57 +0900

    feat : chatGPT의 도움을 받아 모달 confirm, notification 기능 제작

--- src/components/modal/combination/survey/elements/Survey.jsx
+++ src/components/modal/combination/survey/elements/Survey.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import { Form, Input, Radio, Button, Modal, Select, Divider } from 'antd';
+import React, { useState }from 'react';
+import { Form, Input, Radio, Button, Modal, Select, Divider, notification } from 'antd';
 import { useMenuStore } from '../../../../../stores/menu/menu';
 import { useModalStore } from '../../../../../stores/common/common';
+import { GOOD_REASONS, BAD_REASONS } from '../../../../../constants/constans';
 import axios from 'axios';
 
 
-
-
 const Survey = () => {
+    const [form] = Form.useForm();
+
     // 오늘의 메뉴 정보 불러오기
     const { todayMenu } = useMenuStore(); 
 
@@ -17,18 +18,58 @@ const Survey = () => {
     // 모달 닫기
     const closeModal = () => setIsOpen(false);
 
-    // 폼 데이터 처리 구문
-    const onSubmit = (data) => {
-        axios.post(`api/survey/insert`,{
-            stuno: data.stuno,
-            erating: data.erating,
-            detailSurvey: data.mrating.map((menus, index) => menus + data.mreason[index]).join('|')
-        }).then((res) => {
-            console.log(res)
-        })
-        closeModal();
+    // 토스트 메시
+    const openNotification = () => {
+        notification.success({
+          message: '제출 완료',
+          description: '설문조사가 성공적으로 제출되었습니다.',
+        });
       };
 
+    const onSubmit = (data) => {
+        showConfirm(data); // 확인 알림
+    };
+
+    const showConfirm = (formData) => {
+        Modal.confirm({
+            title: '입력한 내용이 확실합니까?',
+            content: `학번과 설문조사 내용을 올바르게 기입해주세요`,
+          onOk() {
+            axios.post(`api/survey/insert`,{
+                stuno: formData.stuno,
+                erating: formData.erating,
+                detailSurvey: formData.mrating.map((menus, index) => menus + formData.mreason[index]).join('|')
+            }).then(() => {
+              openNotification(); // Toast 메시지
+              closeModal();
+            });
+            form.resetFields(); // 폼 리셋
+          },
+        });
+      };
+
+
+    // // 폼 데이터 처리 구문
+    // const onSubmit = (data) => {
+        // axios.post(`api/survey/insert`,{
+        //     stuno: data.stuno,
+        //     erating: data.erating,
+        //     detailSurvey: data.mrating.map((menus, index) => menus + data.mreason[index]).join('|')
+    //     }).then((res) => {
+    //         openNotification();
+    //     })
+    //     form.resetFields();
+    //     closeModal();
+    //   };
+
+    const [selectedOptions, setSelectedOptions] = useState({}); // 추가된 코드
+
+    const handleRadioChange = (e, index) => { 
+        const value = e.target.value.split('_')[1]; 
+        const updatedOptions = { ...selectedOptions };
+        updatedOptions[index] = value.includes("나쁨") ? 'bad' : 'good';
+        setSelectedOptions(updatedOptions);
+      };
 
     return (
         <Modal 
@@ -39,7 +80,7 @@ const Survey = () => {
             footer={null}
         >
             {/* 폼 입력 시작 */}
-            <Form layout='vertical' onFinish={onSubmit}>
+            <Form layout='vertical' onFinish={onSubmit} form={form}>
 
                 {/* 학번 입력 시작*/}
                 <Form.Item 
@@ -82,49 +123,46 @@ const Survey = () => {
                         <span>{menu}</span>
 
                         <Form.Item 
-                            name={['mrating', index]}
-                            rules={[{ required: true, message: '메뉴 만족도를 선택해주세요.' }]}
+                        name={['mrating', index]}
+                        rules={[{ required: true, message: '메뉴 만족도를 선택해주세요.' }]}
                         >
-                            <Radio.Group buttonStyle='solid' style={{marginTop:'15px'}}>
-                                <Radio.Button value={menu + "_매우나쁨_"}>매우나쁨</Radio.Button>
-                                <Radio.Button value={menu + "_나쁨_"}>나쁨</Radio.Button>
-                                <Radio.Button value={menu + "_좋음_"}>좋음</Radio.Button>
-                                <Radio.Button value={menu + "_매우좋음_"}>매우좋음</Radio.Button>
-                            </Radio.Group>
+                        {/* 메뉴 만족도 버튼 시작 */}
+                        <Radio.Group 
+                            buttonStyle='solid' 
+                            style={{marginTop:'15px'}}
+                            onChange={(e) => handleRadioChange(e, index)} // 추가된 코드, by chatGPT
+                        >
+                            <Radio.Button value={menu + "_매우나쁨_"}>매우나쁨</Radio.Button>
+                            <Radio.Button value={menu + "_나쁨_"}>나쁨</Radio.Button>
+                            <Radio.Button value={menu + "_좋음_"}>좋음</Radio.Button>
+                            <Radio.Button value={menu + "_매우좋음_"}>매우좋음</Radio.Button>
+                        </Radio.Group>
                         </Form.Item>
-                        
+                        {/* 메뉴 만족도 버튼 끝 */}
+
+                        {/* 만족도에 따른 이유 선택란 시작 */}
                         <Form.Item 
-                            name={['mreason', index]} 
-                            rules={[{ required: true, message: '이유를 선택해주세요.' }]}
+                        name={['mreason', index]} 
+                        rules={[{ required: true, message: '이유를 선택해주세요.' }]}
                         >
-                                <Select 
-                                    allowClear
-                                    options={[
-                                        {
-                                            label: '좋았다면?',
-                                            options: [
-                                                {lable: '배식량이 만족스러웠다' , value: '배식량이 만족스러웠다'},
-                                                {lable: '간이 적절했다', value: '간이 적절했다'},
-                                                {lable: '메뉴가 맛있었다', value: '메뉴가 맛있었다'},
-                                                {lable: '좋아하는 메뉴다' , value: '좋아하는 메뉴다'}
-                                            ],
-                                        },
-                                        {
-                                            label: '좋지 못했다면?',
-                                            options: [
-                                                { lable: '너무 싱겁다', value: '너무 싱겁다' },
-                                                { lable: '너무 짜다', value: '너무 짜다' },
-                                                { lable: '양이 적다', value: '양이 적다' },
-                                                { lable: '싫어하는 메뉴다', value: '싫어하는 메뉴다' }
-                                            ],
-                                        }
-                                    ]}
-                                    placeholder="만족도 선택의 이유는 무엇인가요?"
-                                />
+                        {/* 조건부 렌더링 , By chatGPT */}
+                        {
+                        selectedOptions[index] === 'good' ? (
+                            <Select allowClear options={GOOD_REASONS} placeholder="좋았다면?"/>
+                        ) : 
+                        selectedOptions[index] === 'bad' ? (
+                            <Select allowClear options={BAD_REASONS} placeholder="좋지 못했다면?"/>
+                        ) : 
+                        (
+                            <Select disabled placeholder="만족도 선택의 이유는 무엇인가요?" />
+                        )}
                         </Form.Item>
-                        <Divider/>
+                        {/* 만족도에 따른 이유 선택란 끝 */}
+
+                        <Divider />
+
                     </div>
-                ))}
+                    ))}
                 {/* 메뉴별 급식 만족도 입력 끝 */}
 
                 {/* 제출 버튼 */}
--- src/constants/constans.js
+++ src/constants/constans.js
@@ -1,2 +1,15 @@
 export const TODAY_SERVER_MODAL_TITLE = `오늘의 급식 설문조사`;
 
+export const GOOD_REASONS = [
+    { label: '배식량이 만족스러웠다', value: '배식량이 만족스러웠다' },
+    { label: '간이 적절했다', value: '간이 적절했다' },
+    { label: '메뉴가 맛있었다', value: '메뉴가 맛있었다' },
+    { label: '좋아하는 메뉴다', value: '좋아하는 메뉴다' }
+  ];
+  
+  export const BAD_REASONS = [
+    { label: '너무 싱겁다', value: '너무 싱겁다' },
+    { label: '너무 짜다', value: '너무 짜다' },
+    { label: '양이 적다', value: '양이 적다' },
+    { label: '싫어하는 메뉴다', value: '싫어하는 메뉴다' }
+  ];
